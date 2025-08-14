45aca607a051b2862bd3101f9fee66e67c7802da
Grecia Delgado <greciadm2596@gmail.com>
2025-08-13 19:57:15 -0600

    file syste exmaple

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 import fs from "node:fs";
- 
-fs.readFile("./src/notes.json", (err, content) => {
+import { console } from "node:inspector";
+
+// EJEMPLO DE LECTURA DE UN ARCHIVO JSON
+// fs.readFile("./src/notes.json","utf8", (err, content) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(content);
+// });
+
+// EJEMPLO PARA ESCRIBIR ARCHIVOS JSON
+// const newNote = { id: 123, content: "Nueva nota 123" };
+
+// fs.writeFile("./src/notes.json", JSON.stringify(newNote), (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+// });
+
+const path = "./src/notes.json";
+const newNote = { id: 1234, content: "Nueva nota 123" };
+
+fs.readFile(path, "utf8", (err, content) => {
   if (err) {
     console.log(err);
     return;
   }
-  console.log(content);
+
+  const notes = JSON.parse(content);
+  notes.push(newNote);
+
+  fs.writeFile(path, JSON.stringify(notes), (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  });
 });
